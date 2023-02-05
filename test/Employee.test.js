83ab7ffe223e1e2f2536eec8c32777cf4b036128
Jacob Jeffries <jeffrij85@gmail.com>
2023-02-05 15:13:54 -0500

    rewrote Eomployee.js & test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,36 +2,11 @@ const Employee = require('../lib/employee')
 
 describe('Employee', () => {
   it('Creates a new Employee class, from input object', () => {
-    const obj = {
-      title: ['Manager'],
-      fname: 'Bilbo',
-      lname: 'Baggins',
-      id: '007',
-      email: 'bilbo_baggins@shire.com',
-      office: '1 Baggend Way'
-    }
 
-    const results = new Employee(obj);
+    const results = new Employee('Bilbo Baggins', '007', 'bilbo@shire.com');
+    
     console.log(results);
 
     expect(results instanceof Employee);
-  })
-
-  it('Should have a postion parameter.', () => {
-    const obj = {
-      title: ['Manager'],
-      fname: 'Bilbo',
-      lname: 'Baggins',
-      id: '007',
-      email: 'bilbo_baggins@shire.com',
-      office: '1 Baggend Way'
-    }
-
-    const results = new Employee(obj);
-
-    console.log(`Expect: ${obj.title}`);
-    console.log(`Results: ${results.title}`);
-
-    expect(results.title).toEqual(obj.title);
   })
 });
