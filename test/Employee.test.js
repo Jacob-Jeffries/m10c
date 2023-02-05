95da790ddfd75592c30528aff1909860d557ec2b
Jacob Jeffries <jeffrij85@gmail.com>
2023-02-05 15:21:36 -0500

    Employee created and passed initial tests.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,12 +1,22 @@
 const Employee = require('../lib/employee')
 
 describe('Employee', () => {
-  it('Creates a new Employee class, from input object', () => {
+  it('Creates a new Employee class, from input', () => {
 
     const results = new Employee('Bilbo Baggins', '007', 'bilbo@shire.com');
     
     console.log(results);
 
     expect(results instanceof Employee);
+  })
+
+  describe('getRole', () => {
+    it('Returns Employee', () => {
+      const results = new Employee('Bilbo Baggins', '007', 'bilbo@shire.com').getRole();
+
+      console.log(results);
+
+      expect(results).toEqual('Employee');
+    })
   })
 });
