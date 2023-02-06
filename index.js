9a07d82895f5c32db2f3662ca305d2f99fb86d47
Jacob Jeffries <jeffrij85@gmail.com>
2023-02-05 19:43:45 -0500

    Created iq prompt

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Employee = require('./lib/employee');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
+const { identifier } = require('@babel/types');
 
 function main() {
   console.log(`\n                ----------                \n`);
@@ -14,15 +15,59 @@ function main() {
   console.log(`\n                ----------                \n`);
   console.log(`\n\nLet's begin by assigning your Team Manager:\n`);
 
-  inq
-    .prompt([
-
-    ]);
-
+  getInfo('Manager');
 }
 
 function getInfo(role){
-  
+
+  let role_char ='';
+
+  switch(role) {
+    case 'Manager':
+      role_char = 'Office Number';
+      break;
+    case 'Engineer':
+      role_char = 'GitHub Username';
+      break;
+    case 'Intern':
+      role_char = 'School';
+      break;
+    default:
+      role_char = 'Wow Jacob messed up'; 
+  }
+
+  const query = [
+    {
+      type: 'input',
+      message: `What is this employee\'s name?`,
+      name: 'a0',
+    },
+    {
+      type: 'input',
+      message: 'What is thie employee\'s ID?',
+      name: 'a1',
+    },
+    {
+      type: 'input',
+      message: 'What is this employee\'s Email?',
+      name: 'a2',
+    },
+    {
+      type: 'input',
+      message: `What is thie employee\'s ${role_char}?`,
+      name: 'a3',
+    },
+  ];
+
+  inq
+    .prompt(query)
+    .then((data) => {
+      let response = [role, data.a0, data.a1, data.a2, data.a3];
+      console.log(response);
+      // return response;
+    }
+  );
+
 }
 
 main();
