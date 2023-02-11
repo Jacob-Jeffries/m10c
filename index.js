1da1071642683eacc1fa92b0fb5e103f04a637d3
Jacob Jeffries <jeffrij85@gmail.com>
2023-02-10 20:15:14 -0500

    fs.writefile

--- index.js
+++ index.js
@@ -249,18 +249,18 @@ function generateHTML(team){
       <!--BOOTSTRAP-->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   
-      <!--JQUERY-->
-      <!-- <script src="~https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>-->
-  
-      <!--JAVASCRIPT-->
-      <!-- <script src="./assets/script.js"></script>-->
-  
     </body>
   
   </html>
   `
-
   console.log(index);
-}
+
+  try {
+    fs.writeFile(`./dist/index.html`, index, 'utf8');
+  }catch(err){
+    console.log('File did not write.');
+    console.log(err);
+  };
+};
 
 main();
